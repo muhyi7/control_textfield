e04701fc055f9803013af56091b3380f208acc41
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-03-09 08:36:36 +0700

    Input and Controller Text

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController myController = TextEditingController();
+  String hasil = "HASIL INPUT";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Text Field"),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextField(
+                    controller: myController,
+                    onChanged: (value){
+                      print("ONCHANGE");
+                      hasil = value;
+                    },
+                    onSubmitted: (value){
+                      setState(() {
+                      print(value);
+                      });
+                    },
+                    onEditingComplete: (){
+                      print("EDIT SUCCESS");
+                      print(myController.text);
+                    },
+                  ),
+                  Text(hasil)
+                ],
+              ),
+            ),
+          ),
+      ),
+    );
+  }
+}
